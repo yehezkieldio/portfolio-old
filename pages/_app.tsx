46ad0e6c69e4fb698efc0d7504164c71ef50939e
LichKing112 <yehezkieldio35@gmail.com>
2021-09-24 09:13:12 +0800

    feat: a favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,40 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { appWithTranslation } from "next-i18next";
 
 import "bulma/css/bulma.min.css";
 import "../styles/globals.scss";
 
 function Portfolio({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<meta charSet="UTF-8" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+
+				<title>Yehezkiel Dio — a web developer</title>
+				<link rel="shortcut icon" href="/favicon.ico" />
+
+				<meta name="robots" content="index,follow" />
+				<meta name="googlebot" content="index,follow" />
+				<meta name="google" content="nositelinkssearchbox" />
+				<meta name="google" content="notranslate" />
+
+				<meta
+					name="google-site-verification"
+					content={
+						process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION_TOKEN
+					}
+				/>
+
+				<meta property="og:image" content="/Avatar.jpg" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 export default appWithTranslation(Portfolio);
