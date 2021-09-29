4520d4f7ef39928c04047fa4035be81da1101772
LichKing112 <yehezkieldio35@gmail.com>
2021-09-29 14:29:01 +0800

    feat: some links

--- next.config.js
+++ next.config.js
@@ -2,9 +2,23 @@ const { i18n } = require("./next-i18next.config");
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-	reactStrictMode: true,
-	images: {
-		domains: ["media.discordapp.net"],
-	},
-	i18n,
+    reactStrictMode: true,
+    images: {
+        domains: ["media.discordapp.net"],
+    },
+    i18n,
+    async redirects() {
+    return [
+      {
+        source: '/blog',
+        destination: 'https://blog.yehezkieldio.xyz',
+        permanent: true,
+      },
+      {
+        source: '/schoolwork',
+        destination: 'https://schoolwork.yehezkieldio.xyz',
+        permanent: true,
+      },
+    ]
+  },
 };
