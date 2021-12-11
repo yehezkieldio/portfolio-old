9f90ea00910bb35002fba074af30e7e6fac55eac
Liz <47420407+LichKing112@users.noreply.github.com>
2021-12-11 21:28:51 +0800

    remove commissions

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -23,7 +23,6 @@ export const Projects: NextPage<IProps> = ({ projects, commissions }) => {
         <Default>
             <Section>
                 <List display="Projects" projects={projects} />
-                <List display="Commissions" projects={commissions} />
             </Section>
         </Default>
     );
