79f62733782d6e75b1c6b041acda503b8931381c
yehezkieldio <yehezkieldio35@gmail.com>
2022-05-12 14:46:53 +0800

    refactor: intial setup

---
+++ pages/_error.tsx
@@ -0,0 +1,12 @@
+import { NextPageContext } from "next";
+
+const Error = ({ statusCode }: { statusCode: string }): JSX.Element => {
+    return <p>{statusCode ? `An error ${statusCode} occurred on server` : "An error occurred on client"}</p>;
+};
+
+Error.getInitialProps = ({ res, err }: NextPageContext) => {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+    return { statusCode };
+};
+
+export default Error;
