77e6b0c97bef33848aefd67bf9e02325759786c0
yehezkieldio <yehezkieldio35@gmail.com>
2022-05-25 23:03:44 +0800

    revert: 9d02565

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import Link from "next/link";
+
+export const Navbar = () => {
+    const [isActive, setIsActive] = useState(false);
+
+    return (
+        <header className="page-header">
+            <nav className="navbar is-transparent" role="navigation">
+                <div className="navbar-brand">
+                    <Link href="/" passHref>
+                        <a className="navbar-item" href="/">
+                            yehezkieldio
+                        </a>
+                    </Link>
+                    <a
+                        onClick={() => {
+                            setIsActive(!isActive);
+                        }}
+                        role="button"
+                        className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
+                        aria-label="menu"
+                        aria-expanded="false"
+                        data-target="navbar-menu"
+                    >
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+                <div id="navbar-menu" className={`navbar-menu ${isActive ? "is-active" : ""}`}>
+                    <div className="navbar-end">
+                        <Link href="/technologies" passHref>
+                            <a className="navbar-item" href="/technologies">
+                                technologies
+                            </a>
+                        </Link>
+                        <Link href="/projects" passHref>
+                            <a className="navbar-item" href="/projects">
+                                projects
+                            </a>
+                        </Link>
+                        <Link href="/notes" passHref>
+                            <a className="navbar-item" href="/notes">
+                                notes
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
